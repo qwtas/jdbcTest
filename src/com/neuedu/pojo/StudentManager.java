e82df7fa51111e825f9ca9e937f8f66b08cd90fc
qwtas <3091252115@qq.com>
2019-07-01 15:45:39 +0800

    Initial commit

---
+++ src/com/neuedu/pojo/StudentManager.java
@@ -0,0 +1,23 @@
+package com.neuedu.pojo;
+
+import java.util.*;
+
+public class StudentManager {
+   private Scanner input = new Scanner(System.in);
+   public void showMenu(){
+       System.out.println("********** 欢迎使用学生管理系统 **********");
+       System.out.println("1 *** 添加学生信息 ***");
+       System.out.println("2 *** 删除学生信息 ***");
+       System.out.println("3 *** 修改学生信息 ***");
+       System.out.println("4 *** 查询学生信息 ***");
+       System.out.println("5 *** 查询一名学生 ***");
+       System.out.println("6 *** 退 出 系 统 ***");
+       System.out.println("*** 请输入编号选择功能 ***");
+
+       int key = input.nextInt();
+       switch (key){
+
+       }
+
+   }
+}
---
+++ src/com/neuedu/test/Test.java
@@ -0,0 +1,9 @@
+package com.neuedu.test;
+
+import com.neuedu.pojo.StudentManager;
+
+public class Test {
+    public static void main(String[] args) {
+        new StudentManager().showMenu();
+    }
+}
